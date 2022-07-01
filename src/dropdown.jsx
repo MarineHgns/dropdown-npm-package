c4d7af86272320f2af229c576e7f9f9797d14422
Marine <marine.huygens@hotmail.fr>
2022-07-01 16:11:10 +0200

    first commit

---
+++ src/dropdown.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './index.css'
+
+const Dropdown = ({ label, options, id, handleFormChange }) => {
+    return (
+        <div className='dropdown'>
+            <label>{label}</label><br/>
+                <select className="dropdown-list" autoComplete='country-name' id={id}
+                    onChange={handleFormChange}
+                    required>
+                    {options.map((option) => (
+                        <option value={option.value} key={option.abbreviation}>{option.name}</option>
+                    ))}
+                </select>
+        </div>
+    );
+  };
+  
+  export default Dropdown;
